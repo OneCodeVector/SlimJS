0c10d5909fbe4711d61a042f88fb38262dc0b0e7
OneCodeVector <117361107+OneCodeVector@users.noreply.github.com>
2024-03-04 17:44:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 Snake.CreateBoard(400, 400, 30)
 
-Snake.SetCell(1, 2, "Head");
 Snake.SetCell(new Vec2(3, 4), "Body");
 
+Snake.ClearGrid();
+
 Snake.OnFrame = function() {
-    console.log("Frame");
+    
+    Snake.SetCell(Snake.Head.Position.X, 0, "Head");
+    
+    Snake.ClearBoard();
+    Snake.DrawBackground();
+    Snake.DrawGrid();
+    Snake.DrawDeadSpace();
+    Snake.DrawGridOutline();
 }
 
 Snake.Start();
